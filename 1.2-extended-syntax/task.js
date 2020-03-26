94be89beb60f2bc587424c81441e86ae3321127d
Lana <Shuvaeva.lana@gmail.com>
2020-03-25 17:57:37 -0700

    Task #1

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,22 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a,b,c) {    
+    const d = b ** 2 - 4 * a * c;   
+     
+    let arr = [];    
+    let x;    
+    let y;
+
+      if (d > 0) {        
+        x = (-b + d) / 2 * a;        
+        y = (-b - d) / 2 * a;
+        arr.push(x, y);    
+      } else if (d === 0) {        
+          x = (-b + d) / 2 * a;        
+          arr.push(x);    
+      }  else {        
+          console.log("")    
+      }
+
+        return arr;
 }
 
 function getAverageMark(marks){
