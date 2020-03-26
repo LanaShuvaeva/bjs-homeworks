28efe4c15b929100a1985f90da6b81d001b48162
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2020-03-26 16:40:49 -0700

    Task #1 - first commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,28 @@
+"use strict"
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    // const loan = Number(amount) - Number(contribution);
+    
+    
+    const loan = amount - contribution;
+    const today = new Date();
+
+    // Calculating months
+    let diff = (date.getTime() - today.getTime()) / 1000;
+    diff /= (60 * 60 * 24 * 7 * 4);
+    const months = Math.abs(Math.round(diff));
+
+    // Calculating monthly payments
+    const payment = loan * ((1/12 * percent / 100) + (1/12 * percent / 100) / (((1 + (1/12 * percent / 100)) ** months) - 1));
+    
+    const totalAmount = (payment * months).toFixed(2);
+
+    console.log(loan);
+    console.log(months);
+    console.log(payment);
+    console.log(totalAmount);
+
+    return Number(totalAmount);
 }
 
 function getGreeting(name) {
