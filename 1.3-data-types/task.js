26f79c7fb432171ed0ee7d7a5d6982824e00b7ec
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2020-03-26 17:12:31 -0700

    Task #1: added iteration over arguments of the function

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,28 +2,33 @@
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // const loan = Number(amount) - Number(contribution);
-    
-    
-    const loan = amount - contribution;
-    const today = new Date();
+    for (let i = 0; i < arguments.length; i++) {
+        if(isNaN(arguments[i])) {
+            alert(`One of the parameters is not a number. Please try again.`);
+        } else {
+            const loan = amount - contribution;
+            const today = new Date();
 
-    // Calculating months
-    let diff = (date.getTime() - today.getTime()) / 1000;
-    diff /= (60 * 60 * 24 * 7 * 4);
-    const months = Math.abs(Math.round(diff));
+            // Calculating months
+            let diff = (date.getTime() - today.getTime()) / 1000;
+            diff /= (60 * 60 * 24 * 7 * 4);
+            const months = Math.abs(Math.round(diff));
 
-    // Calculating monthly payments
-    const payment = loan * ((1/12 * percent / 100) + (1/12 * percent / 100) / (((1 + (1/12 * percent / 100)) ** months) - 1));
-    
-    const totalAmount = (payment * months).toFixed(2);
+            // Calculating monthly payments
+            const payment = loan * ((1/12 * percent / 100) + (1/12 * percent / 100) / (((1 + (1/12 * percent / 100)) ** months) - 1));
+            
+            const totalAmount = (payment * months).toFixed(2);
 
-    console.log(loan);
-    console.log(months);
-    console.log(payment);
-    console.log(totalAmount);
+            console.log(loan);
+            console.log(months);
+            console.log(payment);
+            console.log(totalAmount);
 
-    return Number(totalAmount);
+            return Number(totalAmount);
 }
+        }
+
+    }  
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
