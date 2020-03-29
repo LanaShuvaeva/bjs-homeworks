9f442d22c388513af8a5f7f716df34d4efd9d2d6
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2020-03-28 23:00:15 -0700

    Task #1 - In progress

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -3,26 +3,51 @@
 function getSolutions(a, b, c) {
     const d = b ** 2 - 4 * a * c;
     let x1, x2;
-    if (d < 0) {
-        return `{ D: ${d}, roots: []}`;
-    } else if (d === 0) {
+    let roots = [];
+    if (d === 0) {
         x1 = -1 * b / (2 * a);
-        return `{ D: ${d}, roots: [${x1}]}`;
-    } else {
+        roots.push(x1);
+    } else if (d > 0) {
         x1 = ((-1 * b + Math.sqrt(d)) / (2 * a));
         x2 = ((-1 * b - Math.sqrt(d)) / (2 * a));
-        return `{ D: ${d}, roots: [x1: ${x1}, x2 ${x2}]}`;
+        roots.push(x1, x2);
     }
+    return {
+        D: d,
+        roots
+    };
 }
 
 
 
-function showSolutionsMessage() {
-    getSolutions.apply(null, arguments);
-    console.log(`Вычисляем корни квадратного уравнения  ${a} * (x ** 2) + b * x + c`);
+function showSolutionsMessage(a, b, c) {
+    let result = getSolutions(a, b, c);
+    return result;
 }
 
 
-showSolutionsMessage(getSolutions(2, 4, 2));
+// showSolutionsMessage(1, 2, 3);
 console.log(getSolutions(2, 4, 2));
+console.log(showSolutionsMessage());
 
+// Task #2
+
+
+// function getSolutions(a, b, c) {
+//     const d = b ** 2 - 4 * a * c;
+//     let x1, x2;
+//     if (d < 0) {
+//         return `{ D: ${d}, roots: []}`;
+//     } else if (d === 0) {
+//         x1 = -1 * b / (2 * a);
+//         return `{ D: ${d}, roots: [${x1}]}`;
+//     } else {
+//         x1 = ((-1 * b + Math.sqrt(d)) / (2 * a));
+//         x2 = ((-1 * b - Math.sqrt(d)) / (2 * a));
+//         return `{ D: ${d}, roots: [x1: ${x1}, x2 ${x2}]}`;
+//     }
+//     return {
+//         D: d,
+//         roots: x1, x2;
+//     }
+// }
