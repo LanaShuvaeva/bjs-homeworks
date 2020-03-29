0e2a9286324335028974e7abf79f15d691d32700
Lana <Shuvaeva.lana@gmail.com>
2020-03-29 11:17:33 -0700

    Edited Task #1 & 2, Task #1 is still not working properly

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,17 +1,16 @@
 "use strict"
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // const loan = Number(amount) - Number(contribution);
-    for (let i = 0; i < arguments.length; i++) {
-        if(isNaN(arguments[i])) {
-            alert(`One of the parameters is not a number. Please try again.`);
+        const arr = Object.values(arguments).slice(0, 3);
+        console.log(arr);
+        if(arr.some(isNaN)) {
+            return alert(`One of the parameters is not a number. Please try again.`);
         } else {
             const loan = amount - contribution;
             const today = new Date();
 
             // Calculating months
-            let diff = (date.getTime() - today.getTime()) / 1000;
-            diff /= (60 * 60 * 24 * 7 * 4);
+            let diff = ((date.getTime() - today.getTime()) / 1000) /  (60 * 60 * 24 * 7 * 4);
             const months = Math.abs(Math.round(diff));
 
             // Calculating monthly payments
@@ -25,17 +24,9 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
             console.log(totalAmount);
 
             return Number(totalAmount);
-}
         }
-
-    }  
+}  
 
 function getGreeting(name) {
-    let greeting;
-    if (name !== null && name !== undefined && name !== "") {
-        greeting = `Привет, мир! Меня зовут ${name}`;
-    } else {
-        greeting = `Привет, мир! Меня зовут Аноним`;
-    }
-    return greeting;
+    return `Привет, мир! Меня зовут ${name || "Аноним"}`;
 }
