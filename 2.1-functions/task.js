e559b6896f35ac9d62922d283518312022d3d46c
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2020-03-29 20:57:55 -0700

    Added Task #3

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,3 +1,5 @@
+"use strict"
+
 // Task #1
 
 function getSolutions(a, b, c) {
@@ -45,35 +47,77 @@ function showSolutionsMessage(a, b, c) {
 let avScore = new Object();
 
 function getAverageScore(data) {
-    for (let key in data) {
-        let avg = (data[key].reduce((previous, current) => current += previous)) / data[key].length;
-        avScore[key] = avg;
+    for (let val in data) {
+        let avg = (data[val].reduce((previous, current) => current += previous)) / data[val].length;
+        avScore[val] = avg;
     }      
- 
-    let arr = Object.values(avScore);
 
     function getAverageMark(marks) {
-    
-        for (let key in marks) {
             let avg = (marks.reduce((previous, current) => current += previous)) / marks.length;
             avScore.average = avg;
-        }   
     }; 
 
-    getAverageMark(arr);
+    getAverageMark(Object.values(avScore));
 
     return avScore;
 }
 
-console.log(getAverageScore({
-    algebra: [2, 4, 5, 2, 3, 4],
-    geometry: [2, 4, 5],
-    russian: [3, 3, 4, 5],
-    physics: [5, 5],
-    music: [2, 2, 6],
-    english: [4, 4, 3],
-    poetry: [5, 3, 4],
-    chemistry: [2],
-    french: [4, 4]
+// console.log(getAverageScore({
+//     algebra: [2, 4, 5, 2, 3, 4],
+//     geometry: [2, 4, 5],
+//     russian: [3, 3, 4, 5],
+//     physics: [5, 5],
+//     music: [2, 2, 6],
+//     english: [4, 4, 3],
+//     poetry: [5, 3, 4],
+//     chemistry: [2],
+//     french: [4, 4]
+// }));
+
+
+// Task #3
+
+let pirate = new Object();
+
+function getPersonData(secretData) {
+    pirate.firstName;
+    pirate.lastName;
+        for (let i = 0; i < Object.values(secretData).length; i++){
+        if (Object.values(secretData)[0] === 0) {
+            pirate.firstName = "Родриго";
+        } else if (Object.values(secretData)[0] === 1) {
+            pirate.firstName = "Эмильо";
+        }
+        if (Object.values(secretData)[1] === 0) {
+            pirate.lastName = "Родриго";
+        } else if (Object.values(secretData)[1] === 1) {
+            pirate.lastName = "Эмильо";
+        }
+    }    
+    return pirate;
+}
+
+
+
+console.log(getPersonData({
+    aaa: 0,
+    bbb: 0
 }));
+
+console.log(getPersonData({
+    aaa: 1,
+    bbb: 0
+}));
+
+console.log(getPersonData({
+    aaa: 0,
+    bbb: 1
+}));
+
+console.log(getPersonData({
+    aaa: 1,
+    bbb: 1
+}));
+
+
 
