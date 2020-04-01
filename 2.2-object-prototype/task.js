799b92b97857f5b1cea879f58a3553e26845c23b
Lana <Shuvaeva.lana@gmail.com>
2020-03-31 21:49:16 -0700

    Calculated both dates in Task #3

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -15,6 +15,19 @@ function getAverageMark(marks) {
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
+    const now = Date.now();
+    new Date();
+    // this.birthday = Math.round(birthday.getTime()/1000);
+    const arr = birthday.split("-").map((num) => parseInt(num));
+    const year = arr[0];
+    const month = arr[1];
+    const day = arr[2];
+    this.birthday = new Date(Date.UTC(year, month-1, day));
+    const diff = now - birthday;
+
+    console.log(birthday);
+    console.log(now);
+    console.log(Math.round(this.birthday.getTime()));
+    this.birthday;
     // return verdict
 }
