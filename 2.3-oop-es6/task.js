886c67e0b6fc587253a1e004ee1152184006e0ee
Lana <Shuvaeva.lana@gmail.com>
2020-04-05 14:36:29 -0700

    Started working on the Task #3

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -28,13 +28,8 @@ class Weapon {
 
 class ImprovedWeapon extends Weapon {
     constructor(name, version, attack, durability, range) {
-        super (durability);
-        
-        this.name = name;
+        super (name, attack, durability, range);
         this.version = version;
-        this.attack = attack;
-        this.range = range;
-        this.durability = durability;
     }
 }
 
@@ -49,48 +44,39 @@ const longBow = new ImprovedWeapon('Длинный лук', 'Лук', 15, bow.du
 const poleAxe = new ImprovedWeapon('Секира', 'Меч', 27, 800, sword.range);
 const superStick = new ImprovedWeapon('Посох Бури', 'Посох', 10, stick.durability, 3);
 
-// console.log(longBow.takeDamage(200));
-// console.log(longBow.getDamage());
-console.log(longBow);
-console.log(poleAxe);
-console.log(superStick);
-
-
-// sword.takeDamage(5);
-// console.log(sword.durability); // 5
-
-// sword.takeDamage(50);
-// console.log(sword.durability); // 0
-
-// arm.takeDamage(20);
-// console.log(arm.durability); // Infinity
-
-
-// bow.takeDamage(20);
-// console.log(bow.durability); // 180
-
-// bow.takeDamage(200);
-// console.log(bow.durability); // 0
-
-
-
-// console.log(bow.durability); // 0
-// console.log(bow.getDamage()); // 0
-
-// console.log(arm.durability); // Infinity
-// console.log(bow.getDamage()); // 1
-
-
-// console.log(bow.durability); // 0
-// console.log(bow.isBroken()); // true
-
-// console.log(arm.durability); // Infinity
-// console.log(bow.isBroken()); // false
-
-
-
-
 
 
 // Task #3
 
+class StudentLog {
+    constructor (name) {
+        this.name = name;
+    }
+
+    getName() {
+       return this.name;
+    }
+
+    addGrade(grade, subject) {
+        this.subject.push(grade);
+       console.log();
+
+        // this.geometry = [];
+
+        if (grade >= 1 && grade <= 5) {
+            // this.subject.push(grade);
+            // console.log(this.algebra);
+        } else {
+            console.log(`Вы пытались поставить оценку ${grade}" по предмету "${subject}". Допускаются только числа от 1 до 5.`)
+        }
+
+        return this.algebra;
+    }
+}
+
+const log = new StudentLog('Олег Никифоров');
+
+console.log(log.getName());
+log.addGrade(2, 'algebra');
+log.addGrade(5, 'algebra');
+console.log(log);
