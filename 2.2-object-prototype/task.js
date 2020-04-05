091507f38003164bfd25508663480385b2ab44b3
Lana <Shuvaeva.lana@gmail.com>
2020-04-05 08:29:24 -0700

    Improved functions

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,17 +1,13 @@
 // "use strict"
 
 function getAnimalSound(animal) {
-    this.sound = animal.sound;
-    if (animal === undefined) {
-        return null;
-    } 
-    return sound;
+   return animal === undefined ? null : animal.sound;
 }
 
 function getAverageMark(marks) {
     const average = (marks.map((num) => parseInt(num)).reduce((a, b) => a + b)) / marks.length;
     const roundedAverage = Math.round(average);
-    return roundedAverage;
+    return marks.length > 1 ? roundedAverage : 0;
 }
 
 function checkBirthday(birthday) {
@@ -20,12 +16,8 @@ function checkBirthday(birthday) {
     this.birthday = Math.round(new Date(Date.UTC(arr[0], arr[1]-1, arr[2])).getTime());
     
     const diff = now - this.birthday;
-
     const age = Math.floor((diff/86400000) / 365.25);
-    console.log(age);
 
-    if (age >= 18) {
-        return true;
-    } 
-        return false;
+    return age >= 18;
 }
+
